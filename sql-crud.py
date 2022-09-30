7a36b2529450ef97cc638ce471fd6dad6a62aa16
Henry Leon <henryatusabes@gmail.com>
2022-09-30 23:50:13 +0000

    Learned how to update and delete data

--- sql-crud.py
+++ sql-crud.py
@@ -84,25 +84,77 @@ henry_leon = Programmer(
     first_name="Henry",
     last_name="Leon",
     gender="M",
-    nationality="British",
-    famous_for="Achieving the first 3 projects at Code Institute"
+    nationality="Latin-American",
+    famous_for="Achieving the first three projects at Code Institute"
 )
 
 # add each instance of our programmers to our session
-# session.add(ada_lovelace)
+# session.add(ada_lovelace)
 # session.add(alan_turing)
 # session.add(grace_hopper)
 # session.add(margaret_hamilton)
 # session.add(bill_gates)
 # session.add(tim_berners_lee)
-session.add(henry_leon)
+# session.add(henry_leon)
+
+# updating a single record
+# programmer = session.query(Programmer).filter_by(id=7).first()
+# programmer.famous_for = "World President"
 
 # commit our session to the database
-session.commit()
+# session.commit()
+
+# updating multiple records
+# people = session.query(Programmer)
+# for person in people: 
+#    if person.gender == "F": 
+#        person.gender = "Female" 
+#    elif person.gender == "M":
+#        person.gender = "Male"
+#    else:
+#        print("Gender not defined")
+#    session.commit()
+
+
+# deleting a single record
+# fname = input("Enter a first name: ")
+# lname = input("Enter a last name: ")
+# programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+# defensive programming
+# if programmer is not None:
+#    print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+#    confirmation = input("Are you sure you want to delete this record? (y/n) ")
+#    if confirmation.lower() == "y":
+#        session.delete(programmer)
+#        session.commit()
+#        print("Programmer has been deleted")
+#    else:
+#        print("Programmer not deleted")
+# else:
+#    print("No records found")
+
+
+# delete multiple/all records
+# programmers = session.query(Programmer)
+# for programmer in programmers:
+#     session.delete(programmer)
+#     session.commit()
+# defensive programming
+# if programmer is not None:
+#     print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+#     confirmation = input("Are you sure you want to delete this record? (y/n) ")
+#     if confirmation.lower() == "y":
+#         session.delete(programmer)
+#         session.commit()
+#         print("Programmer has been deleted")
+#     else:
+#         print("Programmer not deleted")
+# else:
+#     print("No records found")
 
 # query the database to find all Programmers
 programmers = session.query(Programmer)
-for programmer in programmers:
+for programmer in programmers: 
     print(
         programmer.id,
         programmer.first_name + " " + programmer.last_name,
@@ -110,4 +162,4 @@ for programmer in programmers:
         programmer.nationality,
         programmer.famous_for,
         sep=" | "
-    )
+        )
